1330a6063f66043c190e17c852e5196c442d0dfa
Илья Рыжков <ryzhkov.ilya17@gmail.com>
2021-10-06 17:55:24 +0300

    done_2

--- api/core/tf_idf.py
+++
@@ -1,9 +0,0 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import pairwise_distances
-
-def tfidf (diction: dict) -> dict:
-    tfidf = TfidfVectorizer(ngram_range=(1, 4))
-    tfidf_representation = tfidf.fit_transform(diction.values())
-    artists_similarity = 1 - pairwise_distances(tfidf_representation, metric="cosine")
-    dict_of_artist_similarity = dict(zip(diction.keys(), artists_similarity))
-    return dict_of_artist_similarity
