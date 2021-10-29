6fac954e32510cdcff41f4c9dbd962e286e67bc8
Илья Рыжков <ryzhkov.ilya17@gmail.com>
2021-10-29 14:15:10 +0300

    del

--- musicrecomend/api/checkers/input_checker.py
+++
@@ -1,6 +0,0 @@
-def input_checker(name):
-    try:
-        isinstance(name, (int, str))
-        return name
-    except:
-        print('error')
--- musicrecomend/bot/api/checkers/input_checker.py
+++
@@ -1,8 +0,0 @@
-def input_checker(name:str):
-    '''Функция принимает на вход текст, проверят имеет ли введенный текст запрещенные символы или нет.'''
-    
-    try:
-        isinstance(name, (int, str))
-        return name
-    except:
-        print('error')
